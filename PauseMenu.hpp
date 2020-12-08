0738a49500ee27d58b3bf45b7fdebfa482b8f967
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2020-12-08 14:58:42 -0800

    Add files via upload

--- PauseMenu.hpp
+++ PauseMenu.hpp
@@ -2,7 +2,14 @@
 #define __PAUSEMENU_HPP__
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include "opencv2\imgproc.hpp"
 using namespace std;
+using namespace cv;
 
 //utilize the singleton pattern
 class PauseMenu
@@ -11,10 +18,13 @@ class PauseMenu
         static PauseMenu* instance;
         //private constructor
         PauseMenu(){};
+        //~PauseMenu();
     public:
         static PauseMenu* getInstance();
-        void outputMenu();
-        //void save();
+        int userControl();
+        string outputMenu(int img);
+        void resume();
+        void save(int x, int y);
 };
 
 #endif
