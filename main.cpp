f0b8dc9baa889f2adb1f6b65054d49a171dbdf92
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2020-12-08 15:00:09 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 #include <iostream>
 
 int main() {
-    //PauseMenu* menu = menu->getInstance();
-    //menu->userControl();
+    PauseMenu* menu = menu->getInstance();
+    menu->userControl();
     //menu->outputMenu();
     //menu->resume();
     //menu->save(1, 4);
 
-    MinigameOptions* m = new MinigameOptions();
-    m->userControl();
+    //MinigameOptions* m = new MinigameOptions();
+    //m->userControl();
 
     //Player* player = new Player();
     //Minigame_Compositor* mc = new Minigame_Compositor("play", player);
