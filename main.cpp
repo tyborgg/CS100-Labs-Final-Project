24947cb929ffdf5739ea6864857b70e79cadfe20
tyborgg <72320023+tyborgg@users.noreply.github.com>
2020-12-10 00:51:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>		
 #include <string>
 #include <sstream>
-#include "start.h"
-#include "maze.h"
+#include "start.hpp"
+#include "maze.hpp"
+#include "PauseMenu.hpp"
+#include "minigameOptions.hpp"
+#include "Minigame_Compositor.hpp"
+#include "mockPlayer.hpp"
 
 using namespace std;
 using namespace cv;
+
 
 int main() {
 	Start start;
 	int input;
 	int choice;
+	int pause;
+	int mini;
+	PauseMenu pauseM;
+	MinigameOptions game;
+	Player* p1 = new Player();
+	Minigame_Compositor* comp1 = new Minigame_Compositor();
+	
 	
 	choice = start.userControl();
 
@@ -18,8 +30,6 @@ int main() {
 		cout << "read in save coordinate from .txt file" << endl; 
 	}
 	else if (choice == 0) {
-		cout << "start new game" << endl;
-
 		ofstream start;
 		start.open("coordinate.txt");
 		start << "0 0";
@@ -34,12 +44,28 @@ int main() {
 
 	while (input != 0) {
 		if (input == 1) {			
-			cout << "pause" << endl;
+			pause = pauseM.userControl();
+			if (pause == 5) {
+				input = maze.userControl();
+			}
+			else if (pause == 6) {
+				input = maze.userControl();
+			}
+			else {
+				input = 0;
+			}
 		}
 		else if (input == 2) {		
-			cout << "mini game" << endl;
+			mini = game.userControl();
+			if (mini == 2) {
+				comp1->play(p1);
+				input = maze.userControl();
+			}
+			else {
+				comp1->skip(p1);
+				input = maze.userControl();
+			}
 		}
-		input = maze.userControl();
 	}
 
 	return 0;
