d1bf97e9e277f4e6ddb61592d5cf4fdac69aa7ce
daniel-nis <72157039+daniel-nis@users.noreply.github.com>
2020-12-09 20:45:54 -0800

    Update displayPMTest.cpp

--- daniel-tests/displayPMTest.cpp
+++ daniel-tests/displayPMTest.cpp
@@ -6,6 +6,7 @@
 
 int main() {
     PauseMenu* menu = menu->getInstance();
+    cout << menu->userControl() << endl;   //tests whether it returns the right number
     menu->userControl();
 
     return 0;
