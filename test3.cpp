a2ea623acb5ca45eb25672c49eea9125fcccaa63
tyborgg <tpast001@ucr.edu>
2020-12-09 23:19:54 -0800

    Test 3 is the same as test 2 except the first time user is set to 'yes' and the control menu is prompted

---
+++ test3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>		
+#include <string>
+#include <sstream>
+#include "start.h"
+#include "maze.h"
+#include "PauseMenu.h"
+#include "minigameOptions.h"
+#include "Minigame_Compositor.h"
+#include "mockPlayer.h"
+
+using namespace std;
+using namespace cv;
+
+int main() {	
+	Start start;
+	int count;
+
+	count = start.userControl();
+
+	if (count == 0) {
+		cout << "Start Game" << endl << "TEST PASSED" << endl;
+	}
+	else {
+		cout << "Start Game" << endl << "TEST PASSED" << endl;
+	}
+
+	return 0;
+}
